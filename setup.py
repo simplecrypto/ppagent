58f05bae100f7d8f2eb28b4e54f284f9cad055b7
Isaac Cook <isaac@simpload.com>
2014-03-09 22:19:25 -0500

    Added image for guide

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open(os.path.join(here, 'README.rst')) as f:
+        README = f.read()
+except:
+    README = ''
 
 
 setup(name='ppagent',
       version='0.1',
       description='A statistics collection agent for powerpool mining server',
       author='Isaac Cook',
+      long_description=README,
       author_email='isaac@simpload.com',
       url='http://www.python.org/sigs/distutils-sig/',
       packages=find_packages(),
