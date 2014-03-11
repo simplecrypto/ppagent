86c64091bf2b871fe4123cda41a868ef3431ae62
Isaac Cook <isaac@simpload.com>
2014-03-11 18:43:19 -0500

    Fix compat with 2.7 by only requiring argparse on 2.6

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 from setuptools import setup, find_packages
 import os
+import sys
 
 here = os.path.abspath(os.path.dirname(__file__))
 try:
@@ -11,15 +12,20 @@ except:
     README = ''
 
 
+requires = []
+# add argparse to be installed for earlier versions of python
+if sys.version_info[:2] <= (2, 6):
+    requires.append('argparse')
+
 setup(name='ppagent',
-      version='0.2.2',
+      version='0.2.3',
       description='A statistics collection agent for powerpool mining server',
       author='Isaac Cook',
       long_description=README,
       author_email='isaac@simpload.com',
       url='http://www.python.org/sigs/distutils-sig/',
       packages=find_packages(),
-      install_requires=['argparse'],
+      install_requires=requires,
       package_data={'ppagent': ['install/*']},
       entry_points={
           'console_scripts': [
